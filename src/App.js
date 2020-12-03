76eecc9d0e13ae45189d3437c2d99a43bd3a9a1c
Jeyoung Park <jason9380@naver.com>
2020-12-03 17:56:08 +0900

    2020/12/03까지의 작업 내용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Potato from "./Potato"; // -> ./는 현재 파일(App.js)와 같은 경로에 있다는 것을 의미
+
+function Food(props){ // props : Food 태그 안에 들어 있는 것
+  return <h1>I like {props.favorite}</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello!</h1>
+      <Food favorite="kimchi"  />
+      <Food favorite="kimchi2"  />
+      <Food favorite="kimchi3"  />
     </div>
   );
 }
